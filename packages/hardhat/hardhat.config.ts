e2991d91c8558a1e80d3d81ed85ac43fa69ba81a
jotagep <jotagepweb@gmail.com>
2021-07-05 18:09:11 +0200

    Add settings, config & rename frontend

--- packages/hardhat/hardhat.config.ts
+++ packages/hardhat/hardhat.config.ts
@@ -1,5 +1,5 @@
 import 'dotenv/config';
-import { HardhatUserConfig} from 'hardhat/types';
+import { HardhatUserConfig } from 'hardhat/types';
 import 'hardhat-deploy';
 import 'hardhat-deploy-ethers';
 import 'hardhat-typechain';
@@ -7,60 +7,56 @@ import 'solidity-coverage';
 
 let mnemonic = process.env.MNEMONIC;
 if (!mnemonic) {
-  mnemonic = 'test test test test test test test test test test test junk';
+	mnemonic = 'test test test test test test test test test test test junk';
 }
 const mnemonicAccounts = {
-  mnemonic,
+	mnemonic,
 };
 
 const accounts = {
-  Localnet: [String(process.env.LOCALNET_PRIVATE_KEY)],
-  Testnet: [String(process.env.TESTNET_PRIVATE_KEY)],
-  Mainnet: [String(process.env.MAINNET_PRIVATE_KEY)],
+	Localnet: [String(process.env.LOCALNET_PRIVATE_KEY)],
+	Testnet: [String(process.env.TESTNET_PRIVATE_KEY)],
+	Mainnet: [String(process.env.MAINNET_PRIVATE_KEY)],
 };
 
+// This adds support for typescript paths mappings
+import 'tsconfig-paths/register';
+
 const config: HardhatUserConfig = {
-  solidity: {
-    version: '0.8.4',
-  },
-  namedAccounts: {
-    deployer: 0,
-  },
-  networks: {
-    coverage: {
-      url: 'http://localhost:5458',
-      accounts: mnemonicAccounts,
-    },
-    hardhat: {
-      accounts: mnemonicAccounts,
-    },
-    localhost: {
-      url: 'http://localhost:8545',
-      accounts: mnemonicAccounts,
-    },
-    localnet: {
-      url: 'http://localhost:9500',
-      chainId: 1666700000,
-      accounts: accounts.Localnet,
-    },
-    testnet: {
-      url: 'https://api.s0.b.hmny.io',
-      chainId: 1666700000,
-      accounts: accounts.Testnet,
-    },
-    mainnet: {
-      url: 'https://api.s0.t.hmny.io',
-      chainId: 1666600000,
-      accounts: accounts.Mainnet,
-    },
-  },
-  paths: {
-    sources: 'src',
-  },
-  external: {
-    contracts: [],
-    deployments: {},
-  },
+	solidity: {
+		version: '0.8.4',
+	},
+	namedAccounts: {
+		deployer: 0,
+	},
+	networks: {
+		coverage: {
+			url: 'http://localhost:5458',
+			accounts: mnemonicAccounts,
+		},
+		hardhat: {
+			accounts: mnemonicAccounts,
+		},
+		localhost: {
+			url: 'http://localhost:8545',
+			accounts: mnemonicAccounts,
+		},
+		localnet: {
+			url: 'http://localhost:9500',
+			chainId: 1666700000,
+			accounts: accounts.Localnet,
+		},
+		testnet: {
+			url: 'https://api.s0.b.hmny.io',
+			chainId: 1666700000,
+			accounts: accounts.Testnet,
+		},
+		mainnet: {
+			url: 'https://api.s0.t.hmny.io',
+			chainId: 1666600000,
+			accounts: accounts.Mainnet,
+		},
+	},
 };
 
 export default config;
