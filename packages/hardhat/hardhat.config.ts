b3be1d11205423063e57f59b7ccd28d7c227bcdd
jotagep <jotagmzn@gmail.com>
2021-07-17 17:01:54 +0200

    Add balance & send tasks

--- packages/hardhat/hardhat.config.ts
+++ packages/hardhat/hardhat.config.ts
@@ -1,9 +1,13 @@
 import 'dotenv/config';
 import { HardhatUserConfig } from 'hardhat/types';
+import { task } from "hardhat/config";
 import '@typechain/hardhat';
 import '@nomiclabs/hardhat-ethers';
 import '@nomiclabs/hardhat-waffle';
 import "hardhat-watcher";
+
+import { fromBech32, toBech32 } from '@harmony-js/crypto';
+import { fromWei, isBech32Address, Units, toWei, numberToHex } from '@harmony-js/utils';
 
 let mnemonic = process.env.MNEMONIC;
 if (!mnemonic) {
@@ -21,6 +25,7 @@ const accounts = {
 
 // This adds support for typescript paths mappings
 import 'tsconfig-paths/register';
+
 
 const config: HardhatUserConfig = {
 	solidity: {
@@ -54,5 +59,54 @@ const config: HardhatUserConfig = {
 		}
 	},
 };
+
+// Tasks
+const parseOneAddress = (address: string): string =>
+	isBech32Address(address) ? fromBech32(address) : address
+
+task("balance", "Prints an account's balance")
+	.addPositionalParam("account", "The account's address")
+	.setAction(async (taskArgs, { ethers }) => {
+		const address = parseOneAddress(taskArgs.account);
+		const balance = await ethers.provider.getBalance(address)
+		console.log('💵 Balance: ' + fromWei(balance.toString(), Units.one), 'ONE')
+	});
+
+task("send", "Send ONE to another account")
+	.addPositionalParam("from", "From account's address")
+	.addPositionalParam("to", "To account's address")
+	.addPositionalParam("amount", "Amount to send in ONE")
+	.addOptionalParam("data", "Data included in transaction")
+	.addOptionalParam("gasPrice", "Price you are willing to pay in gwei")
+	.addOptionalParam("gasLimit", "Limit of how much gas to spend")
+	.setAction(async (taskArgs, { ethers, network }) => {
+		const { from, to, amount, data, gasPrice, gasLimit } = taskArgs;
+		const signer = await ethers.getSigner(parseOneAddress(from));
+		console.log('Sending transaction...')
+		try {
+			const txResponse = await signer.sendTransaction({
+				from: parseOneAddress(from),
+				to: parseOneAddress(to),
+				value: numberToHex(toWei(amount, Units.one)),
+				nonce: await signer.getTransactionCount(),
+				chainId: network.config.chainId,
+				...gasPrice && { gasPrice: numberToHex(toWei(gasPrice, Units.Gwei))  },
+				...gasLimit && { gasLimit },
+				...data && { data }
+			});
+			console.log('Transaction Done ✅')
+			console.log('==================================');
+			console.log(`txHash: ${txResponse.hash}`);
+			console.log(`from: ${toBech32(txResponse.from)}`);
+			console.log(`to: ${txResponse.to ? toBech32(txResponse.to): 'null'}`);
+			console.log(`ammount: ${fromWei(txResponse.value.toString(), Units.one)} ONE`);
+			txResponse.gasPrice && console.log(`gasPrice: ${fromWei(txResponse.gasPrice.toString(), Units.one)} ONE`);
+			console.log('==================================');
+		} catch (error) {
+			console.log('Transaction Failed ❌')
+			console.error(`ERROR: ${error.reason} // ${error.code} -> ${error.value}`)
+		}
+
+	});
 
 export default config;
